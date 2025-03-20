4cf4f992aa2e95a2f1e79d0b6d3cfa7119543e55
belovnv <blovnv@gmail.com>
2025-03-20 10:54:41 +0300

    Исправление стилей

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("css");
-    eleventyConfig.addPassthroughCopy("assets");
-    eleventyConfig.addPassthroughCopy("icons");
-    return {
-      dir: {
-        input: ".",
-        output: "_site"
-      }
-    };
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("css");
+  eleventyConfig.addPassthroughCopy("assets");
+  eleventyConfig.addPassthroughCopy("icons");
+
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+    },
+    pathPrefix: "/my-cv/",
   };
+};
